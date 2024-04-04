9a4518d49195cac4e3ce0ddf519c65be72c557c3
chonglye chang <chonglye@aartkorea.com>
2024-04-04 17:20:34 +0900

    POST / GET

--- back-end/http/SimpleHttpServer.java
+++ back-end/http/SimpleHttpServer.java
@@ -2,8 +2,10 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
 
 public class SimpleHttpServer {
     public static void main(String[] args) throws IOException {
@@ -17,11 +19,23 @@ public class SimpleHttpServer {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello, World!";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            System.out.println("Method : " + t.getRequestMethod());
+            if ("GET".equals(t.getRequestMethod())) {
+                // GET 요청 처리
+                String response = "Hello, World!";
+                t.sendResponseHeaders(200, response.getBytes().length);
+                try (OutputStream os = t.getResponseBody()) {
+                    os.write(response.getBytes());
+                }
+            } else if ("POST".equals(t.getRequestMethod())) {
+                // POST 요청 처리
+                InputStream is = t.getRequestBody();
+                String response = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+                t.sendResponseHeaders(200, response.getBytes().length);
+                try (OutputStream os = t.getResponseBody()) {
+                    os.write(response.getBytes());
+                }
+            }
         }
     }
 }
